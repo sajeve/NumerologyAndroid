bbf9304719e451b23f33d35c730845e70c400a5b
Chukobyte <chukobyte@gmail.com>
2014-04-08 23:54:23 -0400

    Integrated with AdMob

--- src/com/chukobyte/numerology/MainActivity.java
+++ src/com/chukobyte/numerology/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-//import com.google.android.gms.*;
 
 public class MainActivity extends Activity {
 	
@@ -17,7 +16,6 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		PACKAGE_NAME = getApplicationContext().getPackageName();
-//		AdView av = 
 	}
 
 	@Override
